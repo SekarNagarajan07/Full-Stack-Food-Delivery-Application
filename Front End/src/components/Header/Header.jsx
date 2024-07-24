b374e4bd4b2a675a2bceb8c5154c41f218dc7765
Sekar Nagarajan <132734677+SekarNagarajan07@users.noreply.github.com>
2024-07-24 21:51:27 +0530

    jsx

---
+++ Front End/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Craving something delicious? Look no further! Order your favorite food here and indulge in a variety of mouthwatering dishes. Whether you're in the mood for a classic burger, a hearty pizza, or a fresh and healthy salad, we've got you covered. Our easy-to-use platform makes it simple to browse menus, customize your order, and have it delivered straight to your door. Satisfy your hunger and treat yourself to a delightful meal today!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
