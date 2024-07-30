c2f9afb4c8fcb92b354fcbb5a32192803902fd71
Sekar Nagarajan <132734677+SekarNagarajan07@users.noreply.github.com>
2024-07-30 21:51:29 +0530

    store fooditems

---
+++ Front End/src/context/StoreContext.jsx
@@ -0,0 +1,49 @@
+import React, { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: prev[itemId] ? prev[itemId] + 1 : 1,
+    }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      const updatedCartItems = { ...prev };
+      if (updatedCartItems[itemId] > 1) {
+        updatedCartItems[itemId] -= 1;
+      } else {
+        delete updatedCartItems[itemId];
+      }
+      return updatedCartItems;
+    });
+  };
+
+
+
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
