1a199aa14e9895ce94b1650c6e38b4ab7e5c056f
Sekar Nagarajan <132734677+SekarNagarajan07@users.noreply.github.com>
2024-07-30 21:50:48 +0530

    update food items

---
+++ Front End/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { menu_list } from '../../assets/assets'
+import './ExploreMenu.css'
+const ExploreMenu = ({ category, setCategory }) => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>Explore our menu and discover a variety of dishes made with fresh, high-quality ingredients. Each item is crafted to perfection, ensuring a memorable dining experience. Browse through our offerings and find your next favorite dish today!</p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)} key={index} className='explore-menu-list-item'>
+              <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt="" />
+              <p>{item.menu_name}</p>
+            </div>
+          )
+        })}
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+export default ExploreMenu
--- Front End/src/pages/Home/Home.jsx
+++ Front End/src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import './Home.css'
+import React, { useState } from 'react';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import Header from '../../components/Header/Header';
+import './Home.css';
 function Home() {
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   )
 }
