8a33822c0ad67fd6ce0c956dfeb708257ff0ef04
Sekar Nagarajan <132734677+SekarNagarajan07@users.noreply.github.com>
2024-08-06 23:17:09 +0530

    create a cart page

--- Front End/src/App.jsx
+++ Front End/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/' element={<Cart />} />
+          <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
 
         </Routes>
--- Front End/src/context/StoreContext.jsx
+++ Front End/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
@@ -26,18 +26,28 @@ const StoreContextProvider = (props) => {
   };
 
 
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
 
+
+    }
+
+    return totalAmount;
+  }
 
   const contextValue = {
     food_list,
     cartItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount
   };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
 
   return (
     <StoreContext.Provider value={contextValue}>
